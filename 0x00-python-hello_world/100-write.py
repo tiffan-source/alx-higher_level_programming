f08fec82af4716aa2f5ec346a1d96956df227fdc
Tiffane <zohoungbogboanlyou@gmail.com>
2022-08-22 19:18:58 +0100

    write exit correction

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
+sys.stderr.buffer.write(b'and that piece of art ')
+sys.stderr.buffer.write(b'is useful - Dora Korpar, 2015-10-19\n')
 sys.exit(1)
