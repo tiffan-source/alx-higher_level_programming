729293677ba69a01275ec022e782b5bd0262e851
Tiffane <zohoungbogboanlyou@gmail.com>
2022-08-23 13:28:49 +0100

    fix task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    print(f"{'0' if i < 10 else ''}{i:d}", end=('\n' if i == 99 else ", "))
+    prefix_0 = '0' if i < 10 else ''
+    print("{}{}".format(prefix_0, i), end=('\n' if i == 99 else ", "))
