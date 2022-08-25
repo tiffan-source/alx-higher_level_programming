8c4467e897ba3a92e6075e39bcf9c72d766b6f40
Tiffane <zohoungbogboanlyou@gmail.com>
2022-08-25 17:23:07 +0100

    add _name_ task1 correction

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import calculator_1 as calcul
 
-if __name__ == "__main__"
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-print("{} + {} = {}".format(a, b, calcul.add(a, b)))
-print("{} - {} = {}".format(a, b, calcul.sub(a, b)))
-print("{} * {} = {}".format(a, b, calcul.mul(a, b)))
-print("{} / {} = {}".format(a, b, calcul.div(a, b)))
+    print("{} + {} = {}".format(a, b, calcul.add(a, b)))
+    print("{} - {} = {}".format(a, b, calcul.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calcul.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calcul.div(a, b)))
