4a51bdcfcf78773f53d0dd82cd5c522528a9abe8
Tiffane <zohoungbogboanlyou@gmail.com>
2022-08-25 17:27:57 +0100

    another import form

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import calculator_1 as calcul
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, calcul.add(a, b)))
-    print("{} - {} = {}".format(a, b, calcul.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calcul.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calcul.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
