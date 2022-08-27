bbad532d69d9d1b2a1fbff0932da81f26b08e14f
tiffan-source <zohoungbogboanlyou@gmail.com>
2022-08-27 02:52:32 -0700

    try 102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,5 +6,11 @@ import dis
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
-        c = print("CFV", "vf")
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
+            
 print(dis.dis(magic_calculation))
