2858e34e1181ce3527aceb015c72c7e9c3028a5d
Tiffane <zohoungbogboanlyou@gmail.com>
2022-08-27 15:19:22 +0100

    bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-import dis
-
-
+from dis import dis
 
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
@@ -12,5 +10,3 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
-            
-print(dis.dis(magic_calculation))
---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+a = 'a', 'b'
+print(a)
