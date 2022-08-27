ff0621d9203e572113ce2dfe0a5af0be6f9da305
Tiffane <zohoungbogboanlyou@gmail.com>
2022-08-27 10:23:08 +0100

    magic calculation

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import dis
+
+
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = print("CFV", "vf")
+print(dis.dis(magic_calculation))
