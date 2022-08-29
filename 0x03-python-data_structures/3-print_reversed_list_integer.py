2d498bb66e060dcecfcdfe274c842fcb56087c45
Tiffane <zohoungbogboanlyou@gmail.com>
2022-08-29 10:21:51 +0100

    check instance

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        return
-    else:
-        print_reversed_list_integer(my_list[1:])
-        print("{:d}".format(my_list[0]))
+    if isinstance(my_list, list):
+        if len(my_list) == 0:
+            return
+        else:
+            print_reversed_list_integer(my_list[1:])
+            print("{:d}".format(my_list[0]))
