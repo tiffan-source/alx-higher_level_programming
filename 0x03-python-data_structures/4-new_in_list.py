12682528bb71be760e7eb7eacae45fa614d61d8e
Tiffane <zohoungbogboanlyou@gmail.com>
2022-08-29 11:50:41 +0100

    new in list correction fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     if isinstance(my_list, list):
         c_lst = my_list[:]
-        if idx < 0 or idx >= len(my_list):
+        if not (idx < 0 or idx >= len(my_list)):
             return (c_lst)
         c_lst[i] = my_list[idx]
         return (c_lst)
