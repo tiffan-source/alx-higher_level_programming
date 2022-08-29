34a094a638cb39a55890961ed28a9f2420d868e7
Tiffane <zohoungbogboanlyou@gmail.com>
2022-08-29 09:58:52 +0100

    recusrion method

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    l_lst = len(my_list)
-    for i in range(l_lst):
-        print("{:d}".format(my_list[l_lst - i - 1]))
+    if len(my_list) == 0:
+        return
+    else:
+        print_reversed_list_integer(my_list[1:])
+        print("{:d}".format(my_list[0]))
