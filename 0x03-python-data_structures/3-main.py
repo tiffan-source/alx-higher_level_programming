a15973ee8bbca9db1ce16cd8d89e988274a2e73b
Tiffane <zohoungbogboanlyou@gmail.com>
2022-08-29 09:44:49 +0100

    fix reverse mode printing

---
+++ 0x03-python-data_structures/3-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+my_list = [1, 8, 3, 4, 5]
+print_reversed_list_integer(my_list)
