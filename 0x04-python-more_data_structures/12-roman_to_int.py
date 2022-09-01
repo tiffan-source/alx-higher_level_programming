4b35bc43395e75b02b1c9850ee2662dab03d9792
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-01 02:13:38 +0100

    0 for no string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,12 @@ def roman_to_int(roman_string):
     lst = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     result = 0
     prev = 2000
-    for i in roman_string:
-        result += lst[i]
-        if (lst[i] > prev):
+    if isinstance(roman_string, str):
+        for i in roman_string:
+            result += lst[i]
+            if (lst[i] > prev):
             result -= prev * 2
-        prev = lst[i]
-    return result
+            prev = lst[i]
+        return result
+    else:
+        return 0
