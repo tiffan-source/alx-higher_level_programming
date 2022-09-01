2bc77a708222df565f519d7dd7b7e930abb48d85
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-01 02:10:18 +0100

    roman integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def roman_to_int(roman_string):
+    lst = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    result = 0
+    prev = 2000
+    for i in roman_string:
+        result += lst[i]
+        if (lst[i] > prev):
+            result -= prev * 2
+        prev = lst[i]
+    return result
