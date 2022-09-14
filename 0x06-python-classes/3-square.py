a72115270d597bb1183b8175cb67c18599b25449
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-14 02:18:36 +0100

    function area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+File descrie a square class
+"""
+
+
+class Square:
+    """Class Square to represente a square and implement some method
+
+    Attributes:
+    __size (int): Size of the square
+    """
+    __size = 3
+
+    def __init__(self, size=0):
+        """Init function use to set atttribute size to the square
+
+        Args:
+        size (int): size of the square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
