83b1339d1e6b2ef555abc20088ef454a43930bf4
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-14 01:59:31 +0100

    change class name

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
-class Square:
+"""
+File descrie a square class
+"""
+
+class Squar:
     """Class Square to represente a square and implement some method
+    It will be more describe late
     """
