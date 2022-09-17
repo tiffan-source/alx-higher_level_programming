75b0881769b6105ff3aa7b7732116489c5b5b9fe
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-17 13:13:05 +0100

    add integer and test set

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
-"""
-Module add_integer define function
-to do add operation and handle Type
-Error
-"""
+"""Module add_integer define function
+to do add operation and handle some
+Exception like TypeError for not
+integer or float argument"""
+
 
 def add_integer(a, b=98):
+    """ add to integer or float
+    a (int): first number
+    b (int): second number (optional)
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
