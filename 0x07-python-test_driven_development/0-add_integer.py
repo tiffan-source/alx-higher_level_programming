178a2f244aa66589a3daa4f57eba2c0d0d46ffc8
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-17 10:31:27 +0100

    set test for tsk 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Module add_integer define function
+to do add operation and handle Type
+Error
+"""
+
+def add_integer(a, b=98):
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
