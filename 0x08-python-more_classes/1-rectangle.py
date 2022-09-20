8ae080da1bf76c3bfce014a235295102fd2192d2
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-20 03:32:06 +0100

    setter and getter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,43 @@
 #!/usr/bin/python3
 
+"""
+My module define a class Rectangle
+and implement some function and attribute
+to use
+"""
+
+
 class Rectangle:
-    pass
+    """
+    Inside my class I define setter, getter and init for
+    field width and height
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise TypeError("width must be >= 0")
+        else:
+            self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise TypeError("height must be >= 0")
+        else:
+            self.__height = height
