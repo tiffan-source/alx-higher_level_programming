130772f19f1b9e0207b3824e01c8ef2c7b30635a
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-27 19:15:22 +0100

    add description to function

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,6 +5,8 @@ import json
 
 
 def load_from_json_file(filename):
+    """ load_from_json_file - create an object from json file
+    filename: file to read """
     with open(filename, "r", encoding="UTF-8") as f:
-        obj = f.readline()
+        obj = f.readlines()
         return json.loads(obj)
