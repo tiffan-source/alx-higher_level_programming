5f2a10a430d416c32b3a66abda4ac73bab0a9f05
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-27 17:11:50 +0100

    add comment in the function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,10 @@ a file in UTF-8 format"""
 
 
 def read_file(filename=""):
+    """ read_file - reads a text file and
+    prints it to stdout
+    filename: name of the file to read
+    """
     with open(filename, "r", encoding="UTF-8") as f:
         for line in f:
             print(line, end="")
