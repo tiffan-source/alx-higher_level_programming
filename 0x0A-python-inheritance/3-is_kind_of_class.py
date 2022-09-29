2b58a3ea388e6172acaea9734c92446d8612d96d
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-30 00:39:54 +0100

    add try catch to handle no clss in issubclass

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,6 +7,9 @@ def is_kind_of_class(obj, a_cls):
     is_kind_of_class - return True if the object is exaclty
     an instance o sub instance of the specified class and false otherwise
     """
-    if type(obj) in [a_cls] or isinstance(obj, a_cls) or issubclass(obj, a_cls):
-        return True
-    return False
+    try:
+        if type(obj) in [a_cls] or isinstance(obj, a_cls) or issubclass(obj, a_cls):
+            return True
+        return False
+    except TypeError:
+        return False
