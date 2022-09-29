643668bc08ffd4c96676f5d2d1e4386186e1b66d
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-30 00:19:23 +0100

    detect instance or subinstance add type check

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,11 +2,11 @@
 """Module define fonction to determine if isinstance or subinstance"""
 
 
-def is_kind_of_class(obj, a_class):
+def is_kind_of_class(obj, a_cls):
     """
     is_kind_of_class - return True if the object is exaclty
     an instance o sub instance of the specified class and false otherwise
     """
-    if isinstance(obj, a_class) or issubclass(obj, a_class):
+    if type(obj) in [a_cls] isinstance(obj, a_cls) or issubclass(obj, a_cls):
         return True
     return False
