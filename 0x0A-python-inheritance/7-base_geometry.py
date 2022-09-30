1f6510b3e7dc4d99bb24fac837e1b9e87443ec44
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-30 02:33:58 +0100

    7 base geometry add test data

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module define geometry class
+"""
+
+
+class BaseGeometry():
+    """
+    class generic define base geometry form
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise TypeError("{} must be greater than 0".format(name))
