bd8e5e3fdf887b29b45024096ee8247b9ff537fa
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-30 02:19:43 +0100

    area function

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module define geometry class
+"""
+
+
+class BaseGeometry():
+    """
+    class generic define base geometry form
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
