adf77f8cca7e2d354e3e3ff38f5f7d38f9c91ac8
Tiffane <zohoungbogboanlyou@gmail.com>
2022-10-04 03:58:33 +0100

    save to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,23 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        save_to_file - returns the JSON string
+        representation of list_dictionaries
+        list_objs: list of object to add in the list
+        """
+        if list_objs is None:
+            with open("Rectangle.json", "w") as f_j:
+                f_j.write(cls.to_json_string("[]"))
+            with open("Square.json", "w") as f_j:
+                f_j.write(cls.to_json_string("[]"))
+        elif len(list_objs) != 0 and issubclass(type(list_objs[0]), Base):
+            with open(list_objs[0].__class__.__name__ + ".json", "w") as f_j:
+                dct_to_set = []
+                for i in list_objs:
+                    dct_to_set.append(i.to_dictionary())
+
+                f_j.write(cls.to_json_string(dct_to_set))
--- 0x0C-python-almost_a_circle/tests/tests_base_json.py
+++ 0x0C-python-almost_a_circle/tests/tests_base_json.py
@@ -2,13 +2,14 @@
 import unittest
 from models.base import Base
 
+
 class JSON_Base_Test(unittest.TestCase):
 
     def test_doc_json_function(self):
         self.assertIsNotNone(Base.to_json_string.__doc__)
 
     def test_return_type(self):
-        data = [{"id":90}]
+        data = [{"id": 90}]
         self.assertTrue(type(Base.to_json_string(data)) in [str])
 
     def test_value_if_none_or_empty(self):
@@ -16,4 +17,4 @@ class JSON_Base_Test(unittest.TestCase):
         self.assertEqual(Base.to_json_string(None), "[]")
 
     def test_value_if_data(self):
-        self.assertEqual(Base.to_json_string([{"id":80}]), "[{\"id\": 80}]")
+        self.assertEqual(Base.to_json_string([{"id": 80}]), "[{\"id\": 80}]")
