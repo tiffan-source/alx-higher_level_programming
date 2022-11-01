47df68a151d40109300940cbdfa84b73421493d5
Tiffane <zohoungbogboanlyou@gmail.com>
2022-11-02 00:58:29 +0100

    convert to utf8 fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,5 +7,18 @@
 --     Table first_table
 --     Field name in first_table
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE
+      hbtn_0c_0
+      CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE
+      hbtn_0c_0.first_table
+      CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE
+      hbtn_0c_0.first_table
+      CHANGE name
+      CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
