c48a211fda7775ac5023920232083f35ce10b8fb
Tiffane <zohoungbogboanlyou@gmail.com>
2022-11-02 00:42:17 +0100

    alter charset

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,3 +8,4 @@
 --     Field name in first_table
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
