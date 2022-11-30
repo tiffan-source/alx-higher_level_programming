9a81b937ca0ff981ab0592559cbd58540dd6b84c
Tiffane <zohoungbogboanlyou@gmail.com>
2022-11-30 03:25:00 +0100

    print rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,9 +2,17 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    }
+  }
+
+  print () {
+    let i = this.height;
+    while (i > 0) {
+      console.log('X'.repeat(this.width));
+      i--;
     }
   }
 };
