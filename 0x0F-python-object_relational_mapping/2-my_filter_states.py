2a2e5885d058f90b90d2f589af62f7421ed7fe61
Tiffane <zohoungbogboanlyou@gmail.com>
2022-12-04 03:30:29 +0100

    check value in select

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    curs = db.cursor()
+    query = f"SELECT * FROM `states` WHERE `states`.`name` = '{sys.argv[4]}'"
+    curs.execute(query)
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    db.close()
