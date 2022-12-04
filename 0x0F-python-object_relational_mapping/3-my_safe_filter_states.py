9a702132ae45fb3809004ed712a4eaf3fe73156f
Tiffane <zohoungbogboanlyou@gmail.com>
2022-12-04 03:57:52 +0100

    prepare query

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    curs = db.cursor()
+    query = "SELECT * FROM `states`\
+        WHERE `states`.`name` = %s\
+        ORDER BY `states`.`id`", (str(sys.argv[4],))
+    curs.execute(query)
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    db.close()
